b30b94f8b5fa5c3aaf3602795db715a87d323ce3
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2022-10-06 15:40:34 -0300

    Aula if

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if (vel > 60){
+    console.log(`Vc ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando sinto de segurança`)
---
+++ ex009.js
@@ -0,0 +1,8 @@
+var pais = `japão`
+console.log(`Vivendo em ${pais}`)
+if(pais == `Brasil`){
+    console.log(`Vc é Brasileiro`)
+}
+else{
+    console.log(`Vc é estrangeiro`)
+}
