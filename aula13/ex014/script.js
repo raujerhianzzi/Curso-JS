2d275fafbe0d714654ddf9e2ee283164b5d12c47
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2022-10-17 16:35:47 -0300

    atualizei

--- aula13/ex014/script.js
+++ aula13/ex014/script.js
@@ -1,1 +1,21 @@
-alert `Ola`
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('foto') 
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 22
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'manha.png'
+        document.body.style.background = `#F0D4B5`
+    }else if (hora >= 12 && hora < 18) {
+        //BOA TARDE!
+        img.src = 'tarde.png'
+        document.body.style.background = `#FF9400`
+    }else {
+        //BOA NOITE!
+        img.src = 'noite.png'
+        document.body.style.background = `#3D7FB1`
+    }
+}
