a79c6ab96d3f1ea402ed043801833bbba3608c8c
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2022-10-18 16:01:14 -0300

    .

--- aula13/ex014/script.js
+++ aula13/ex014/script.js
@@ -1,9 +1,9 @@
 function carregar(){
     var msg = document.getElementById('msg')
-    var img = document.getElementById('foto') 
+    var img = document.getElementById('imagem') 
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 22
+    var hora = data.getHours()
+    //var hora = 22
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //BOM DIA!
