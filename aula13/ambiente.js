98022e6313e59b32b7bb674c159f0ce1e96f6072
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2022-10-25 14:34:54 -0300

    repeticao

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+while (c <= 30){
+    console.log(`passo ${c}`)
+    c++
+}
+
+
+
+
+
+/*
+console.log('tudo berm ?')
+console.log('tudo berm ?')
+console.log('tudo berm ?')
+console.log('tudo berm ?')
+console.log('tudo berm ?')
+console.log('tudo berm ?')
+*/
