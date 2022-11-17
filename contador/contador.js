00ec2eef16bb21a91601f59bcfe4740e151a96b8
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2022-11-17 15:52:54 -0300

    final exercicio

--- contador/contador.js
+++ contador/contador.js
@@ -20,8 +20,40 @@ function inlista(n, l){
 
 function adicionar(){
     if(isNumero(num.value) && !inlista(num.value, valores)) {
-        alert('Tudo ok.')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     }else {
         alert('Valor invalido ou ja encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        alert('Adicionar valores antes de finalizar!')
+    }else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p> o maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p> o menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p> Somando todos os valores temos ${soma}.</p>`
+        res.innerHTML += `<p> A média de todos o valores digitados é ${media}.</p>`
     }
 }
